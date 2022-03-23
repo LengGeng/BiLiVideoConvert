3dbf94f81662a314ba726c8e5b705724fad17d77
LengGeng <906506482@qq.com>
2022-03-23 14:28:46 +0800

    完善视频信息解析部分

--- BiLiVideoConvert.py
+++ BiLiVideoConvert.py
@@ -1,5 +1,6 @@
-import json
 import os
+import json
+import warnings
 from sys import argv
 from getopt import getopt
 
@@ -16,24 +17,157 @@ class BiLiVideoConvert:
         :param input_dir: 下载视频路径
         :param output_dir: 转换后视频存放路径
         """
+        # 参数为空时读取配置文件，配置文件中不存在则使用默认配置
+        if input_dir is None:
+            input_dir = CONFIG.get("input_dir", "download")
+        if output_dir is None:
+            output_dir = CONFIG.get("output_dir", "output")
         self.input_dir = input_dir
         self.output_dir = output_dir
         self.movie_dirs = os.listdir(input_dir)
-        self.movies = self.get_movie_info()
+        self.movies = {}
+
+    def parse_movies(self):
+        for movie_info in self.get_movie_infos():
+            avid = movie_info.get("avid")
+            if avid:
+                avid = f"AV{avid}"
+            bvid = movie_info["bvid"]
+            season_id = movie_info["season_id"]
+            if season_id:
+                season_id = f"S_{season_id}"
+            vid = avid or bvid or season_id
+            # 不存在添加默认信息
+            if vid not in self.movies:
+                self.movies[vid] = {
+                    "avid": avid,
+                    "bvid": bvid,
+                    "season_id": season_id,
+                    "title": movie_info['title'],  # 标题
+                    "total": 0,  # 总量
+                    "download_total": 0,  # 下载总量
+                    "page_data": []  # 视频Page数据
+                }
+            # 判断视频是否下载完成，添加分P数据
+            is_completed = movie_info['is_completed']  # 是否下载完成
+            self.movies[vid]["total"] += 1
+            page_data = {
+                "page": movie_info["page"],
+                "part": movie_info["part"],
+                "is_completed": is_completed
+            }
+            if is_completed:
+                self.movies[vid]["download_total"] += 1
+                page_data["video_path"] = movie_info["video_path"]
+                page_data["audio_path"] = movie_info["audio_path"]
+            self.movies[vid]["page_data"].append(page_data)
+
+    def get_movie_infos(self) -> dict:
+        """
+        获取 input_dir 下视频项目的信息
+        :return:
+        """
+        for movie_dir in self.movie_dirs:
+            # 拼接视频项目的绝对路径
+            movie_ads_dir = os.path.join(self.input_dir, movie_dir)
+            # 遍历视频项目下的目录
+            for folder_name, sub_folders, file_names in os.walk(movie_ads_dir):
+                entry_file = os.path.join(folder_name, "entry.json")
+                # 以存在entry.json文件为判断视频目录依据
+                if os.path.exists(entry_file):
+                    # 解析 entry 文件
+                    entry = parse_entry(entry_file)
+                    if entry:
+                        yield entry
+                        # if movie_dir == str(entry['vid'])
+
+    def show_info(self):
+        """
+        展示视频信息
+        :return:
+        """
+        movies_list = []
+        for index, [vid, movie] in enumerate(self.movies.items()):
+            movies_list.append(vid)
+            print(f"{index + 1}、[{movie['download_total']:-3}/{movie['total']:-3}]  {movie['title']}")
+
+        in_index: str = input("请输入要转换的编号: ")
+        if in_index == "all":
+            # for movies in movies_list:
+            #     self.create_movie(movies)
+            pass
+        else:
+            # self.create_movie(movies_list[int(in_index) - 1])
+            pass
 
     def run(self):
         """
         主程序
         :return:
         """
+        print("开始解析视频信息...")
+        self.parse_movies()
+        print("解析视频信息完成")
+        self.show_info()
         pass
 
-    def get_movie_info(self) -> dict:
-        """
-        获取 input_dir 下视频项目的信息
-        :return:
-        """
-        pass
+
+def parse_entry(entry_file):
+    """
+    解析视频配置(入口)文件
+    :param entry_file: 文件路径
+    :return: 视频信息
+    """
+    # 打开文件
+    try:
+        with open(entry_file, 'r', encoding='utf-8') as fp:
+            entry: dict = json.load(fp)
+            # 解析媒体类型
+            media_type: int = entry.get('media_type')  # 媒体类型,1的可能是blv格式
+            if media_type not in [2]:
+                # 不支持的媒体类型
+                warnings.warn(f"Warning Unsupported media type:{media_type} in {entry_file}")
+                return
+            # 解析视频 ID
+            avid: int = entry.get('avid')  # avid
+            bvid: str = entry.get('bvid')  # bvid
+            season_id: int = entry.get('season_id')  # season_id, 番剧id
+            # 视频信息
+            title: str = entry.get("title")  # 视频标题
+            is_completed: bool = entry.get("is_completed", False)  # 是否下载完成
+            # 获取当前视频分集的信息数据
+            if avid or bvid:
+                page = entry["page_data"]["page"]  # 视频索引
+                part = entry["page_data"]["part"]  # 视频标题
+            if season_id:
+                page = entry["ep"]["page"]
+                part = entry["ep"]["index_title"]
+
+            item = {
+                "avid": avid,
+                "bvid": bvid,
+                "season_id": season_id,
+                "title": title,
+                "is_completed": is_completed,
+                "page": page,
+                "part": part
+            }
+            # 判断视频下载完成, 获取视频文件及音频文件信息
+            if is_completed:
+                # 视频、音频下载目录
+                type_tag = entry.get('type_tag')
+                # 视频路径
+                video_path = os.path.join(os.path.dirname(entry_file), type_tag, "video.m4s")
+                if os.path.exists(video_path):  # 判断文件是否存在
+                    item["video_path"] = video_path
+                # 音频路径
+                audio_path = os.path.join(os.path.dirname(entry_file), type_tag, "audio.m4s")
+                if os.path.exists(audio_path):  # 判断文件是否存在
+                    item["audio_path"] = audio_path
+            return item
+    except json.decoder.JSONDecodeError as e:
+        # 文件无法解析
+        warnings.warn(f"Warning file could not parse: {entry_file} \n{e.msg}")
 
 
 def get_command_args() -> tuple:
